efa06a8303bc8701939714e842d259dee9ebce80
raneejoshi <raneejoshi@gmail.com>
2022-01-30 07:38:51 +0800

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -4,14 +4,13 @@ import requests
 from datetime import datetime
 import praw
 
+NOTION_API_KEY= os.environ['NOTION_API_KEY']
+REDDIT_CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
+REDDIT_CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
+REDDIT_USER_AGENT = os.environ['REDDIT_USER_AGENT']
 
-NOTION_API_KEY= 'secret_JycCdih4STec1Ba5OJqiBZdKlcdw8iMzBELV8bVCa6v'
-REDDIT_CLIENT_ID = 'notion'
-REDDIT_CLIENT_SECRET = '9Kps6gibdsm8dlImP9KioZoOIDOVaA'
-REDDIT_USER_AGENT =  'V53TNiGno12ao4EfPItL2A'
-
-DATABASE_KEY = '7cfa100e6ac044ceadc81330482e1b8d'
-PAGE_KEY = '118ad8e790fa439bac32a5d78ff98ef3'
+DATABASE_KEY = os.environ['DATABASE_KEY']
+PAGE_KEY = os.environ['PAGE_KEY']
 
 def create_notionpost(title, contenturl, actualurl):
 
@@ -20,17 +19,26 @@ def create_notionpost(title, contenturl, actualurl):
         'Content-Type': 'application/json',
         'Notion-Version': '2021-08-16',
     }
-    data = { "parent": { "database_id": DATABASE_KEY }, "properties": { 
+    post_data = { "parent": { "database_id": DATABASE_KEY }, 
+            "icon": {
+        "type": "emoji",
+        "emoji": '👾'},
+        "properties": { 
         "Title": {"title": [ { "text": { "content": title } } ] },
         #"score": { "number": score },
         #"subreddit": { "select": { "name": subreddit } },
         # "subreddit": { "rich_text": [ { "text": { "content": subreddit } } ] },
         "ContentURL": {"url": contenturl}, 
-        "Link": {"url": actualurl}, 
+        "Link": {"url": actualurl},
+        "Tags": {"multi_select": [{"name": "👨‍👩‍👧‍👦 Community"}]},
+        "Series":{"multi_select": [{"name": "Formula 1"}]}
         #"created": {"date" : {"start": created_date}}, 
     },  }
 
-    requests.post('https://api.notion.com/v1/pages', headers=headers, json=data)
+    requests.post(f'https://api.notion.com/v1/pages', headers=headers, data=json.dumps(post_data)).json()
+    #if c.status_code != 200:
+        #raise RuntimeError(c.content)
+        #print(data)
 
 
 ## checks the notion block and retrieve subreddits in form of list of strings
@@ -44,24 +52,25 @@ def get_subreddits():
     #response = requests.get(f'https://api.notion.com/v1/blocks/{PAGE_KEY}/children?page_size=10', headers=headers)
     #mylist = response.json()['results'][1]['bulleted_list_item']['text'][0]['plain_text'].split()
     #return mylist
-    post_data = {'filter':
-                 {'and':
-                  [
-                      {"property": "Enabled", 
+    post_data = {"filter":
+                 {"and":[
+                    {"property": "Enabled", 
                        "checkbox" : {"equals":True}
                       }, 
                       {"property": "Source", 
-                       "equals" : "Reddit"}
+                       "select": {"equals" : [{"name": "Reddit"}]}
+                      }
                   ]
                  }
                 }
     response  = requests.post(f'https://api.notion.com/v1/databases/{PAGE_KEY}/query', data=json.dumps(post_data), headers=headers).json()
-    print(response)
+    #print(response["results"])
     for i in response["results"]:
         subreddit=i['properties']['Title']['title'][0]['plain_text']
         mylist.append(subreddit)
     return mylist
-
+    #print(mylist)
+    
 def reddit_notion(subreddits):
 
     ## first: retrieve database and create searchable set 
@@ -74,6 +83,7 @@ def reddit_notion(subreddits):
     response = requests.post(f'https://api.notion.com/v1/databases/{DATABASE_KEY}/query', headers=headers)
     myneeded = response.json()['results']
     myset = set()
+    #print(myneeded)
 
     for i in myneeded:
         try:
@@ -86,15 +96,19 @@ def reddit_notion(subreddits):
                         client_secret= REDDIT_CLIENT_SECRET,
                         user_agent= REDDIT_USER_AGENT)
     for subreddit in subreddits:
+        #print(subreddit)
         try:
             #sub=i['Title']
             #test_reddit = reddit.subreddit(sub).top("month", limit = 10)
-            test_reddit = reddit.subreddit(subreddit).top("day", limit = 2)
+            test_reddit = reddit.subreddit(subreddit).top("day")
+            #print(test_reddit)
 
             # test_reddit = reddit.multireddit("reactjs", "programming").top("day")
             # ml_subreddit = reddit.subreddit(mysub)
             for post in test_reddit:
+                #print(post)
                 if ("https://www.reddit.com" + post.permalink) not in myset:
+                    #print(myset)
                     create_notionpost(post.title, post.url, ("https://www.reddit.com" + post.permalink))
         except:
             pass
